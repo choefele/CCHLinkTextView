31ca8814fb830b78d33814ee7b2973beb43d60e7
Aral Balkan <aral@ind.ie>
2015-12-21 17:31:07 +0000

    Added Carthage compatibility.

---
+++ CCHLinkTextView/CCHLinkTextView/CCHLinkTextView.h
@@ -0,0 +1,19 @@
+//
+//  CCHLinkTextView.h
+//  CCHLinkTextView
+//
+//  Created by Aral Balkan on 21/12/2015.
+//  Copyright © 2015 Aral Balkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCHLinkTextView.
+FOUNDATION_EXPORT double CCHLinkTextViewVersionNumber;
+
+//! Project version string for CCHLinkTextView.
+FOUNDATION_EXPORT const unsigned char CCHLinkTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCHLinkTextView/PublicHeader.h>
+
+
